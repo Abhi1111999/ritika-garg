3d4c6cd7ddead534858865612fc951bc9fade291
unknown <abhishek011pandey@gmail.com>
2024-03-22 03:51:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="heading">
+        <h1>welcome to ritikas resume</h1>
+      </div>
+      <div className="heading">
+        <h2>development in progress...</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
